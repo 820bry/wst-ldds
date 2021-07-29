b36afd929f920c136cc1d9b8bcd478b216287384
Vento <kinloketan@gmail.com>
2021-07-29 11:47:14 +0800

    JS Progress

--- scripts/events.js
+++ scripts/events.js
@@ -1,11 +1,14 @@
+var elementToBeDelete;
+
 window.onload = () => {
     document.getElementById("event_delete").addEventListener("click", (e) => {
-        if (e.target != document.getElementById("event_delete_window"))
+        if (!document.getElementById("event_delete_window").contains(e.target))
             unloadConfirmationBox();
     })
 }
 
-function loadConfirmationBox() {
+function loadConfirmationBox(elementFor) {
+    elementToBeDelete = elementFor;
     document.getElementById("event_delete").style.visibility = "visible";
 }
 
@@ -13,12 +16,27 @@ function unloadConfirmationBox() {
     document.getElementById("event_delete").style.visibility = "hidden";
 }
 
-function loadEventOptionBox(x) {
-    x.setAttribute("onclick", "unloadEventOptionBox(this)");
-    x.parentElement.parentElement.firstElementChild.style.visibility = "visible";
+function deleteEvent(bool) {
+    var eventElements = document.getElementsByClassName("event");
+
+    if (bool) {
+        //Delete the event box
+        for (var i = 0; i < eventElements.length; i++) {
+            if (eventElements[i].contains(elementToBeDelete))
+                eventElements[i].remove();
+        }
+        unloadConfirmationBox();
+    } else {
+        unloadConfirmationBox();
+    }
 }
 
-function unloadEventOptionBox(x) {
-    x.setAttribute("onclick", "loadEventOptionBox(this)");
-    x.parentElement.parentElement.firstElementChild.style.visibility = "hidden";
+function loadEventOptionBox(element) {
+    element.setAttribute("onclick", "unloadEventOptionBox(this)");
+    element.parentElement.parentElement.firstElementChild.style.visibility = "visible";
+}
+
+function unloadEventOptionBox(element) {
+    element.setAttribute("onclick", "loadEventOptionBox(this)");
+    element.parentElement.parentElement.firstElementChild.style.visibility = "hidden";
 }
