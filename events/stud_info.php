90f8e594e205bc70e3ac4114a03af5a778e25c98
yannhao <tanyanhao2002@gmail.com>
2021-08-01 20:50:57 +0800

    .

---
+++ events/stud_info.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.grey-orange.min.css" />
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+
+    <link rel="stylesheet" href="../style/main.css">
+    <script src="../scripts/carousel.js" type="text/javascript"></script>
+    <script src="../scripts/events.js" type="text/javascript"></script>
+    <title>Events | LDDS</title>
+</head>
+
+
+<body>
+
+    <div class="banner">
+        <img src="../style/images/ldds-logo.png" alt="logo" />
+        <a href="../">Home</a>
+        <a href="../events/">Events</a>
+        <a href="../committee/">Committee</a>
+        <a href="../about-us/">About Us</a>
+    </div>
+
+
+    <div class="stud_info">
+        <div class="stud">
+
+            <svg class="pic" width="100" height="100" >
+                <circle id="pic" cx="50" cy="50" r="40" fill="grey" />   
+            </svg>
+
+            <span class="name">Name</span>
+            <span class="stud_id">Student ID</span>
+            <img src="../style/images/edit_icon.png" alt="logo" class="pro_icon">
+        </div>
+
+        <div class="info">
+            <div>Email : </div>
+            <div>Faculty :</div>
+            <div>Programme :</div>
+            <div>Tutorial Group:</div>
+            <div>Gender :</div>
+            <div>NRIC :</div>
+
+        </div>
+
+        <div class="cross_tick">
+            <img src="../style/images/cross.png" alt="logo">
+            <img src="../style/images/tick.png" alt="logo">
+        </div>
+        
+    </div>
+</body>
+
+</html>
