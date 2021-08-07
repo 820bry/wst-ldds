439a74c37813c10112d9426f603f576332dc7d64
Bryan Lai <bryanlai820@gmail.com>
2021-08-08 03:09:41 +0800

    events info page

--- events/index.php
+++ events/index.php
@@ -29,7 +29,7 @@ require_once('../banner.php');
             <div class="events-list">
                 <a class="event-box" href="/wst-ldds/events/event_info/">
                     <h3>Event A</h3>
-                    <div class="desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+                    <div class="desc">A Short Description for Event A.</div>
                     <div class="deadline">Deadline: 31/12/2021</div>
                     <div class="slots-left">Slots Left: 69</div>
                 </a>
