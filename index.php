6da3a51e0eb1f24a1a0c5d2fa760b6418758adb9
Vento <kinloketan@gmail.com>
2021-08-07 23:11:36 +0800

    Home animation draft code

--- index.php
+++ index.php
@@ -13,8 +13,10 @@ require_once('banner.php');
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.grey-orange.min.css" />
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <link rel="stylesheet" href="style/main.css">
+    <script src="scripts/home.js" type="text/javascript"></script>
     <script src="scripts/scrollbar.js" type="text/javascript"></script>
     <script src="scripts/carousel.js" type="text/javascript"></script>
     <title>Home | LDDS</title>
---
+++ scripts/home.js
@@ -0,0 +1,11 @@
+// $(document).scroll(() => {
+//     if ($(document).scrollTop() >= 8467) {
+//         $("#home-comm").fadeIn(2000);
+//         console.log($(document).scrollTop());
+//     }
+//     else $("#home-comm").fadeOut();
+
+//     if ($(document).scrollTop() >= 9609) $("#home-event").fadeIn(2000);
+//     else $("#home-event").fadeOut();
+    
+// })
