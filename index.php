cb230632a9e0b80d397f303c588365a3371f79c6
StraXRobinark <raresugarglider@gmail.com>
2021-08-07 18:41:47 +0800

    Added an events section to the homepage Change chenzheng's image to a suitable event image

--- index.php
+++ index.php
@@ -67,6 +67,7 @@ require_once('banner.php');
             </div>
         </div>
         <div class = "home-comm" id = "home-comm">
+            <h1>Committee</h1>
             <div class = "comm-desc" id = "comm-desc">
                 <p>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
@@ -78,6 +79,24 @@ require_once('banner.php');
             </div>
             <div class = "more-comm-info" id = "more-comm-info">
                 <a href = "committee/index.php">
+                    More
+                </a>
+            </div>
+        </div>
+        
+        <div class = "home-event" id = "home-event">
+            <h1>Events</h1>
+            <div class = "event-desc" id = "event-desc">
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                </p>
+            </div>
+            <div class = "event-disp" id = "event-disp">
+                <img src = "style/images/committee/CZ.jpg">
+            </div>
+            <div class = "more-event-info" id = "more-event-info">
+                <a href = "events/index.php">
                     More
                 </a>
             </div>
