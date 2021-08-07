61322ad8cd5240d2c511352b4820a433dd6ddd03
StraXRobinark <raresugarglider@gmail.com>
2021-08-08 01:32:28 +0800

    Fixed Yanhao's fvked up mistake The memberlist.php is curred

--- member_list/member_list.php
+++ member_list/member_list.php
@@ -8,6 +8,7 @@
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.grey-orange.min.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 
     <link rel="stylesheet" href="/wst-ldds/style/member_list.css">
@@ -27,57 +28,67 @@
         <a href="../about-us/">About Us</a>
     </div>
 
-    <div class="head_of_table">
-        <h3 class="head_one">Member</h3>
-            <input type="text" class="searchbar" id="searchbar" placeholder="Search">
+    <div class = "member-container" id = "member-container">
+        <div class="head_of_table">
+            <h3 class="head_one">Members</h3>
+                <input type="text" class="searchbar" id="searchbar" placeholder="Search">
+            <div class = "close-button">
+                <button class = "mdl-button mdl-js-button mdl-button--mini-fab mdl-js-ripple-effect"></button>
+            </div>
+        </div>
+        <div class = "page-controls" id = "page-controls">
+            <span><b>Page</b></span>
+            <span class ="page-count" id = "page-count">1 - 1 of 1</span>
+            <button class="mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect"><i class="material-icons">cached</i></button>
+            <button class="mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect"><i class="material-icons">navigate_before</i></button>
+            <button class="mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect"><i class="material-icons">navigate_next</i></button>
+        </div>
+        <div class="table_list">
+            <table class="member_list">
+                <thead>
+                    <th class = "chckbox"><label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="checkbox-0"><input type="checkbox" id="checkbox-0" class="mdl-checkbox__input"></label></th>
+                    <th>Name</th>
+                    <th>Student ID</th>
+                    <th>Course</th>
+                    <th>Email</th>
+                </thead>
+
+            <tbody>
+                <tr>
+                    <td class = "chckbox"><label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="checkbox-1"><input type="checkbox" id="checkbox-1" class="mdl-checkbox__input"></label></td>
+                    <td>Abu</td>
+                    <td>00PMD00000</td>
+                    <td>DFT</td>
+                    <td>abuabuabu@gmail.com</td>
+                </tr>
+
+                <tr>
+                    <td class = "chckbox"><label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="checkbox-2"><input type="checkbox" id="checkbox-2" class="mdl-checkbox__input"></label></td>
+                    <td>Abu</td>
+                    <td>00PMD00000</td>
+                    <td>DFT</td>
+                    <td>abuabuabu@gmail.com</td>
+                </tr>
+
+                <tr>
+                    <td class = "chckbox"><label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="checkbox-3"><input type="checkbox" id="checkbox-3" class="mdl-checkbox__input"></label></td>
+                    <td>Abu</td>
+                    <td>00PMD00000</td>
+                    <td>DFT</td>
+                    <td>abuabuabu@gmail.com</td>
+                </tr>
+
+                <tr>
+                    <td class = "chckbox"><label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="checkbox-4"><input type="checkbox" id="checkbox-4" class="mdl-checkbox__input"></label></td>
+                    <td>Abu</td>
+                    <td>00PMD00000</td>
+                    <td>DFT</td>
+                    <td>abuabuabu@gmail.com</td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
     </div>
-
-    <div class="table_list">
-        <table class="member_list">
-            <thead>
-                <th><input type="checkbox"></th>
-                <th>Name</th>
-                <th>Student ID</th>
-                <th>Course</th>
-                <th>Email</th>
-            </thead>
-
-        <tbody>
-            <tr>
-                <td><input type="checkbox"></td>
-                <td>Abu</td>
-                <td>00PMD00000</td>
-                <td>DFT</td>
-                <td>abuabuabu@gmail.com</td>
-            </tr>
-
-            <tr>
-                <td><input type="checkbox"></td>
-                <td>Abu</td>
-                <td>00PMD00000</td>
-                <td>DFT</td>
-                <td>abuabuabu@gmail.com</td>
-            </tr>
-
-            <tr>
-                <td><input type="checkbox"></td>
-                <td>Abu</td>
-                <td>00PMD00000</td>
-                <td>DFT</td>
-                <td>abuabuabu@gmail.com</td>
-            </tr>
-
-            <tr>
-                <td><input type="checkbox"></td>
-                <td>Abu</td>
-                <td>00PMD00000</td>
-                <td>DFT</td>
-                <td>abuabuabu@gmail.com</td>
-            </tr>
-        </tbody>
-        </table>
-    </div>
-
 
 
 
