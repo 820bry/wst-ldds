83ae1c3c009aafe679421b87b55ab3c068c3e83e
StraXRobinark <raresugarglider@gmail.com>
2021-08-08 13:34:02 +0800

    final fixes

--- member_list/member_list.php
+++ member_list/member_list.php
@@ -15,12 +15,12 @@ require_once('/wst-ldds/banner.php');
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="../style/main.css"/>
+    <link rel="stylesheet" href="/style/main.css"/>
 
-    <link rel="stylesheet" href="../style/member_list.css">
-    <script src="../scripts/carousel.js" type="text/javascript"></script>
-    <script src="../scripts/events.js" type="text/javascript"></script>
-    <script src="../scripts/member_list.js" type="text/javascript" defer></script>
+    <link rel="stylesheet" href="/style/member_list.css">
+    <script src="/scripts/carousel.js" type="text/javascript"></script>
+    <script src="/scripts/events.js" type="text/javascript"></script>
+    <script src="/scripts/member_list.js" type="text/javascript" defer></script>
     <title>Events | LDDS</title>
 </head>
 
