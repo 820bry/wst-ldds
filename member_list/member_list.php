6ed988432054dc3f0f3df647010d78261830e88e
Vento <kinloketan@gmail.com>
2021-08-10 12:22:38 +0800

    Fixed search bug for member list

--- member_list/member_list.php
+++ member_list/member_list.php
@@ -28,7 +28,7 @@ require_once('../banner.php');
     <div class = "member-container" id = "member-container">
         <div class="head_of_table">
             <h3 class="head_one">Members</h3>
-                <input type="text" class="searchbar" id="searchbar" placeholder="Search" oninput="search(1)">
+                <input type="text" class="searchbar" id="searchbar" placeholder="Search" oninput="search(1, sortBy, sortDirection)">
             
             <span class="select-custom">
                 <select id="choose_item" name="chooseitem" form="item">
@@ -50,13 +50,13 @@ require_once('../banner.php');
         <div class = "page-controls" id = "page-controls">
             <span><b>Page</b></span>
             <span class ="page-count" id = "page-count">1 of 1</span>
-            <button class="mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect" id="refresh" onclick="search(page);"><i class="material-icons">cached</i></button>
+            <button class="mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect" id="refresh" onclick="search(page, sortBy, sortDirection)"><i class="material-icons">cached</i></button>
             <div class="mdl-tooltip" for="refresh">Refresh</div>
             <button class="mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect" id="del-selected" onclick="deleteSelectedRow()"><i class="material-icons">delete</i></button>
             <div class="mdl-tooltip" for="del-selected">Delete Selected</div>
-            <button class="mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect" id="prev" onclick="search(page-1)" disabled><i class="material-icons">navigate_before</i></button>
+            <button class="mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect" id="prev" onclick="search(page-1, sortBy, sortDirection)" disabled><i class="material-icons">navigate_before</i></button>
             <div class="mdl-tooltip" for="prev" id="prev-tooltip">Previous Page</div>
-            <button class="mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect" id="next" onclick="search(page+1)"><i class="material-icons">navigate_next</i></button>
+            <button class="mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect" id="next" onclick="search(page+1, sortBy, sortDirection)"><i class="material-icons">navigate_next</i></button>
             <div class="mdl-tooltip" for="next" id="next-tooltip">Next Page</div>
         </div>
         <div class="table_list">
