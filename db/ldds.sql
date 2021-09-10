4aa13e5748b0bdd3f679f02ab99326c72638d368
Vento <kinloketan@gmail.com>
2021-09-10 17:50:23 +0800

    Updated db file

--- db/ldds.sql
+++ db/ldds.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 30, 2021 at 04:36 PM
+-- Generation Time: Sep 10, 2021 at 11:49 AM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.7
 
@@ -29,23 +29,20 @@ USE `ldds`;
 -- Table structure for table `committee`
 --
 
+DROP TABLE IF EXISTS `committee`;
 CREATE TABLE `committee` (
   `student_id` varchar(10) NOT NULL,
   `position` varchar(20) NOT NULL,
   `image_path` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Truncate table before insert `committee`
---
-
-TRUNCATE TABLE `committee`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `event`
 --
 
+DROP TABLE IF EXISTS `event`;
 CREATE TABLE `event` (
   `id` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
@@ -57,17 +54,13 @@ CREATE TABLE `event` (
   `person_in_charge` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Truncate table before insert `event`
---
-
-TRUNCATE TABLE `event`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `event_registration`
 --
 
+DROP TABLE IF EXISTS `event_registration`;
 CREATE TABLE `event_registration` (
   `registration_id` varchar(45) NOT NULL,
   `event_id` int(11) NOT NULL,
@@ -76,19 +69,16 @@ CREATE TABLE `event_registration` (
   `image_path` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Truncate table before insert `event_registration`
---
-
-TRUNCATE TABLE `event_registration`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `member`
 --
 
+DROP TABLE IF EXISTS `member`;
 CREATE TABLE `member` (
   `name` varchar(30) NOT NULL,
+  `img_path` varchar(30) DEFAULT 'default.png',
   `student_id` varchar(10) NOT NULL,
   `password` varchar(65) NOT NULL,
   `ic_no` varchar(15) NOT NULL,
@@ -100,20 +90,17 @@ CREATE TABLE `member` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Truncate table before insert `member`
---
-
-TRUNCATE TABLE `member`;
---
 -- Dumping data for table `member`
 --
 
-INSERT INTO `member` (`name`, `student_id`, `password`, `ic_no`, `email`, `phone_no`, `faculty`, `course_code`, `permission_level`) VALUES
-('Chiam Jin Qin', '20PMD00000', 'ac8ce7b28dfb17800988a8ff9d14ed269dcc74bc9c7bda2b006037c8da6122b1', '020202-02-0202', 'jinqin@gmail.com', '012345678', 'FOCS', 'DFT', 0),
-('Tan Yan Hao', '20PMD11111', 'ac8ce7b28dfb17800988a8ff9d14ed269dcc74bc9c7bda2b006037c8da6122b1', '010101-01-0101', 'yanhao@gmail.com', '0122345689', 'FOCS', 'DFT', 0),
-('Tan Kin Loke', '20PMD33333', 'ac8ce7b28dfb17800988a8ff9d14ed269dcc74bc9c7bda2b006037c8da6122b1', '030303-03-0303', 'kinloke@gmail.com', '011-1123564', 'FOCS', 'DFT', 0),
-('Bryan Lai', '20PMD44444', 'ac8ce7b28dfb17800988a8ff9d14ed269dcc74bc9c7bda2b006037c8da6122b1', '040404-04-0440', 'bryan@gmail.com', '012-11122355', 'FOCS', 'DFT', 0),
-('Neoh Yi Zhen', '20PMD55555', 'ac8ce7b28dfb17800988a8ff9d14ed269dcc74bc9c7bda2b006037c8da6122b1', '060606-06-0606', 'yizhen@gmail.com', '012-34567897', 'FOCS', 'DFT', 0);
+INSERT INTO `member` (`name`, `img_path`, `student_id`, `password`, `ic_no`, `email`, `phone_no`, `faculty`, `course_code`, `permission_level`) VALUES
+('Chiam Jin Qin', 'default.png', '20PMD00000', 'ac8ce7b28dfb17800988a8ff9d14ed269dcc74bc9c7bda2b006037c8da6122b1', '020202-02-0202', 'jinqin@gmail.com', '012345678', 'FOCS', 'DFT', 0),
+('Chiam Jin Qin', 'default.png', '20PMD00010', 'ac8ce7b28dfb17800988a8ff9d14ed269dcc74bc9c7bda2b006037c8da6122b1', '020202-02-0202', 'chiamjinqin@gmail.com', '017-8888888', 'FOCS', 'DFT', 0),
+('Tan Kin Loke', '20PMD00679.png', '20PMD00679', 'cc51a5bf62531dc53de0553ca28e7ddfdf8e27e0b1c329a39715490d024a13a3', '010101-01-0101', 'kinloketan@gmail.com', '0166682798', 'FOCS', 'DFT', 1),
+('Tan Yan Hao', 'default.png', '20PMD11111', 'ac8ce7b28dfb17800988a8ff9d14ed269dcc74bc9c7bda2b006037c8da6122b1', '010101-01-0101', 'yanhao@gmail.com', '0122345689', 'FOCS', 'DFT', 0),
+('Tan Kin Loke', 'default.png', '20PMD33333', 'ac8ce7b28dfb17800988a8ff9d14ed269dcc74bc9c7bda2b006037c8da6122b1', '030303-03-0303', 'kinloke@gmail.com', '011-1123564', 'FOCS', 'DFT', 0),
+('Bryan Lai', 'default.png', '20PMD44444', 'ac8ce7b28dfb17800988a8ff9d14ed269dcc74bc9c7bda2b006037c8da6122b1', '040404-04-0440', 'bryan@gmail.com', '012-11122355', 'FOCS', 'DFT', 0),
+('Neoh Yi Zhen', 'default.png', '20PMD55555', 'ac8ce7b28dfb17800988a8ff9d14ed269dcc74bc9c7bda2b006037c8da6122b1', '060606-06-0606', 'yizhen@gmail.com', '012-34567897', 'FOCS', 'DFT', 0);
 
 --
 -- Indexes for dumped tables
