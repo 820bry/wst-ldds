d3f016b27f92fa70a799c9c7aca7390812030a57
StraXRobinark <raresugarglider@gmail.com>
2021-09-11 18:46:38 +0800

    I touched banner.css

--- profile/history.php
+++ profile/history.php
@@ -4,11 +4,26 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.grey-orange.min.css" />
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script src="/wst-ldds/scripts/scrollbar.js" type="text/javascript"></script>
+    <script src="/wst-ldds/scripts/profile.js" type="text/javascript"></script>
+    <link  rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="/wst-ldds/style/main.css">
+    <link rel="stylesheet" href="/wst-ldds/style/profile.css">
+
+    <title>My History| LDDS</title>
+</head>
     <body>
         <?php
         // put your code here
