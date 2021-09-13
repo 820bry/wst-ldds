d6cc3f6687d71750155804e55dc53216ad52d8d2
Vento <kinloketan@gmail.com>
2021-09-14 00:29:19 +0800

    Event join fixed

--- events/event_join.php
+++ events/event_join.php
@@ -5,9 +5,8 @@
     if ($_SERVER['REQUEST_METHOD'] == "GET") {
         if (isset($_GET['eventID'])) {
             $id = "test2";
-            $empty = "";
-            $query = $conn->prepare("INSERT INTO ${db_event_registration} VALUES (?,?,?,now(),?)");
-            $query->bind_param("ssss",$id, $_GET['eventID'],$_SESSION['student_id'], $empty);
+            $query = $conn->prepare("INSERT INTO ${db_event_registration} VALUES (?,?,?,now())");
+            $query->bind_param("sss",$id, $_GET['eventID'],$_SESSION['student_id']);
             if ($query->execute()) {
                 echo "Success";
             } else {
